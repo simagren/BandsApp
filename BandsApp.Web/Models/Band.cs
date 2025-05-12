73246d8032f486f886c218ec9f0c4d47d17fc735
Laagren <simonagren@hotmail.se>
2025-05-12 16:09:14 +0200

    Added BandService and Model

---
+++ BandsApp.Web/Models/Band.cs
@@ -0,0 +1,9 @@
+﻿namespace BandsApp.Web.Models
+{
+    public class Band
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ BandsApp.Web/Services/BandService.cs
@@ -0,0 +1,21 @@
+﻿using BandsApp.Web.Models;
+
+namespace BandsApp.Web.Services
+{
+    public class BandService
+    {
+        List<Band> bands = [
+            new Band { Id = 1, Name = "Rammstein", Description = "A heavy metal band" },
+            new Band { Id = 2, Name = "AC/DC", Description = "Rock n Roll band" },
+            new Band { Id = 3, Name = "Iron Maiden", Description = "Heavy metal band"},
+            new Band { Id = 4, Name = "Kent", Description = "Swedish pop band"}
+            ];
+
+        public Band[] GetAllBands() => bands
+            .OrderBy(b => b.Name)
+            .ToArray();
+
+        public Band? GetBandById(int id) => bands
+         .SingleOrDefault(b => b.Id == id);
+    }
+}
