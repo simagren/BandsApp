52ef996ffa32e5a9213fbed94f033ff8f0ca2c29
Laagren <simonagren@hotmail.se>
2025-05-12 16:21:44 +0200

    Added controller and index view

---
+++ BandsApp.Web/Controllers/BandsController.cs
@@ -0,0 +1,24 @@
+﻿using BandsApp.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BandsApp.Web.Controllers;
+
+public class BandsController : Controller
+{
+    BandService bandService = new BandService();
+
+    [Route("")]
+    public IActionResult Index()
+    {
+        var model = bandService.GetAllBands();
+        return View(model);
+    }
+
+
+    [Route("details/{id}")]
+    public IActionResult Details(int id)
+    {
+        var model = bandService.GetBandById(id);
+        return View(model);
+    }
+}
--- BandsApp.Web/Program.cs
+++ BandsApp.Web/Program.cs
@@ -5,9 +5,12 @@ namespace BandsApp.Web
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            app.MapControllers();
 
             app.Run();
         }
