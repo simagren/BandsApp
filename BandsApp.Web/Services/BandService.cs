c33ff81ea9dc3a173efbcbe676ae7f846d1949de
Fredrik Norgren <fredrik_norgren@hotmail.com>
2025-05-13 10:33:42 +0200

    Added albums for each band

--- BandsApp.Web/Services/BandService.cs
+++ BandsApp.Web/Services/BandService.cs
@@ -6,10 +6,10 @@ namespace BandsApp.Web.Services
     public class BandService
     {
         List<Band> bands = [
-            new Band { Id = 1, Name = "Rammstein", Description = "A heavy metal band", ImageUrl = "/Images/rammstein.jpg" },
-            new Band { Id = 2, Name = "AC/DC", Description = "Rock n Roll band", ImageUrl = "/Images/acdc.jpg" },
-            new Band { Id = 3, Name = "Iron Maiden", Description = "Heavy metal band" , ImageUrl = "/Images/ironmaiden.jpg"},
-            new Band { Id = 4, Name = "Kent", Description = "Swedish pop band", ImageUrl = "/Images/kent.jpg"}
+            new Band { Id = 1, Name = "Rammstein", Description = "A heavy metal band", ImageUrl = "/Images/rammstein.jpg", Albums = ["Mutter","Zeit","Rammstein"] },
+            new Band { Id = 2, Name = "AC/DC", Description = "Rock n Roll band", ImageUrl = "/Images/acdc.jpg", Albums = ["Back in Black", "High Voltage", "Highway to hell"] },
+            new Band { Id = 3, Name = "Iron Maiden", Description = "Heavy metal band" , ImageUrl = "/Images/ironmaiden.jpg", Albums =["Fear of the dark", "Powerslave", "The number of the beasty"]},
+            new Band { Id = 4, Name = "Kent", Description = "Swedish pop band", ImageUrl = "/Images/kent.jpg", Albums =["Du & jag döden", "Kent" , "Tillbaka till framtiden"] }
             ];
 
         public Band[] GetAllBands() => bands
