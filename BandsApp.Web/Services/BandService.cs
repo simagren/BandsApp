aa518948b6dac37f02ecca60570b9e85d306683b
Tobias Johansson <tobias@noq.nu>
2025-05-13 09:44:27 +0200

    added images for all bands

--- BandsApp.Web/Services/BandService.cs
+++ BandsApp.Web/Services/BandService.cs
@@ -6,10 +6,10 @@ namespace BandsApp.Web.Services
     public class BandService
     {
         List<Band> bands = [
-            new Band { Id = 1, Name = "Rammstein", Description = "A heavy metal band", ImageUrl = "~/images/rammstein.jpg" },
-            new Band { Id = 2, Name = "AC/DC", Description = "Rock n Roll band", ImageUrl = "Images/acdc.png" },
-            new Band { Id = 3, Name = "Iron Maiden", Description = "Heavy metal band" , ImageUrl = "Images/acdc.png"},
-            new Band { Id = 4, Name = "Kent", Description = "Swedish pop band", ImageUrl = "Images/acdc.png"}
+            new Band { Id = 1, Name = "Rammstein", Description = "A heavy metal band", ImageUrl = "/Images/rammstein.jpg" },
+            new Band { Id = 2, Name = "AC/DC", Description = "Rock n Roll band", ImageUrl = "/Images/acdc.jpg" },
+            new Band { Id = 3, Name = "Iron Maiden", Description = "Heavy metal band" , ImageUrl = "/Images/ironmaiden.jpg"},
+            new Band { Id = 4, Name = "Kent", Description = "Swedish pop band", ImageUrl = "/Images/kent.jpg"}
             ];
 
         public Band[] GetAllBands() => bands
